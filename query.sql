8029f4f88c35ac4a881aea2e99610ed462c1923c
carode <carode21944@gmail.com>
2019-11-11 23:15:16 -0500

    adding comments

--- query.sql
+++ query.sql
@@ -1,7 +1,11 @@
+--Droping all tables
+
 DROP TABLE uk_boys_names;
 DROP TABLE uk_girls_names;
 DROP TABLE us_boys_names;
 DROP TABLE us_girls_names;
+
+--Creating "uk_boys_names" table
 
 CREATE TABLE uk_boys_names(
     Rank int,
@@ -9,11 +13,15 @@ CREATE TABLE uk_boys_names(
    Count INT
 );
 
+--Creating "uk_girls_names" table
+
 CREATE TABLE uk_girls_names (
     Rank INT,
    Name VARCHAR NOT NULL,
    Count INT
 );
+
+--Creating "us_boys_names" table
 
 CREATE TABLE us_boys_names (
    Rank SERIAL PRIMARY KEY,
@@ -21,21 +29,31 @@ CREATE TABLE us_boys_names (
    Number INT
 );
 
+--Creating "us_girls_names" table
+
 CREATE TABLE us_girls_names (
    Rank INT,
    Name VARCHAR NOT NULL,
    Number INT
 );
 
+--Selecting all in "uk_boys_names" table where rank is 68
+
 select *
 from uk_boys_names
 where rank = 68
 
+--Selecting all in "uk_girls_names" table
+
 select *
 from uk_girls_names;
 
+--Selecting all in "us_girls_names" table
+
 select *
 from us_girls_names;
+
+--Selecting all in "us_boys_names" table
 
 select *
 from us_boys_names;
